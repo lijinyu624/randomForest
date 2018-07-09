ccb33f3f46f3c2e1d99a796e766ee5e1445a1594
yupan <impanyu@gmail.com>
2018-07-09 13:53:21 -0500

    fix bugs

--- R/randomForest.default.R
+++ R/randomForest.default.R
@@ -365,7 +365,7 @@ mylevels <- function(x) if (is.factor(x)) levels(x) else 0
 		ymean <- mean(y)
 		y <- y - ymean
 		ytest <- ytest - ymean
-        rfout <- .Call("regRFMultiRes",
+        rfout <- .C("regRFMultiRes",
                     x,
                     #as.double(y),
                     as.integer(c(n, p)),
