01cd64263ad86a5ffbba55dc0c5a6b77e567f657
yupan <impanyu@gmail.com>
2018-07-08 19:31:16 -0500

    fix bugs to regrf.c

--- src/regrf.c
+++ src/regrf.c
@@ -136,7 +136,7 @@ void regRFMultiRes(double *x, int *xdim, int *sampsize,
                  return;
             }
                 //call k times of refRF for one choice of x and y 
-            for(int k=0; k<20; k++){       
+            for(int k=0; k<1; k++){       
                        
                        regRF(xSelected, ySelected, xdimSelected, sampsize,
                          nthsize, nrnodes, nTree,mtry, imp,
