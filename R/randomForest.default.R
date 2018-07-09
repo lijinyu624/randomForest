a12ff761da961225e59416d06bbe9946385b012a
yupan <impanyu@gmail.com>
2018-07-08 22:10:21 -0500

    fix bugs

--- R/randomForest.default.R
+++ R/randomForest.default.R
@@ -440,7 +440,7 @@ mylevels <- function(x) if (is.factor(x)) levels(x) else 0
         out <- list(call = cl,
                     type = "regression",
                     predicted = structure(ypred + ymean, names=x.row.names),
-                    pred= rfout$yprtmtx,
+                    pred= rfout$mse,
                     mse = rfout$mse,
                     rsq = 1 - rfout$mse / (var(y) * (n-1) / n),
                     oob.times = rfout$oob.times,
