8d5551ffef817f40a572a398fd2db2fd42f3a5fb
yupan <impanyu@gmail.com>
2018-07-08 21:51:47 -0500

    fix bugs

--- src/regrf.c
+++ src/regrf.c
@@ -138,7 +138,6 @@ void regRFMultiRes(double *x, int *xdim, int *sampsize,
                  return;
             }
                 //call k times of refRF for one choice of x and y 
-            for(int k=0; k<1; k++){       
                        
                        regRF(xSelected, ySelected, xdimSelected, sampsize,
                          nthsize, nrnodes, nTree,mtry, imp,
@@ -155,7 +154,7 @@ void regRFMultiRes(double *x, int *xdim, int *sampsize,
                                noutAll[s]+=nout[s];
                               }
                        
-           }
+    
    
    }
  }
