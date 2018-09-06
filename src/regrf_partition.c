4292ceba25ed4e0cb3eae03a76b7c4846cf3dd95
lijinyu624 <lijinyu624@gmail.com>
2018-09-06 14:35:10 -0500

    test

--- src/regrf_partition.c
+++ src/regrf_partition.c
@@ -255,14 +255,14 @@ void regRFMultiRes2(double *x, int *xdim, int *sampsize,
 	for(int s = 0; s<nsample; s++)  
         for(int m = 0; m < xdimCount; m++) {
               xSelected[m + s*xdimCount] = x[ninds[xind[m]] + s*mdim];
-			  Rprintf("d,",ninds[xind[m]]);
+			  Rprintf("%d,",ninds[xind[m]]);
           }
 	
       for(int j=0; j<ydimCount + (mdim-j*ydimCount);j++){// select y
                     //yflag[ninds[j]]=1; //flag y
                     for(int n=0; n<nsample;n++) 
 						ySelected[n]=x[ninds[yind[j]]+n*mdim];
-					Rprintf("d,",ninds[yind[j]]);
+					Rprintf("%d,",ninds[yind[j]]);
                    // zeroDouble(yptr, nsample);
                        
                        regRF(xSelected, ySelected, xdimSelected, sampsize,
