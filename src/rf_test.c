2e08a8cb67fed30c0779f898b7edb6c7dc4501b6
lijinyu624 <lijinyu624@gmail.com>
2018-10-09 14:56:38 -0500

    fix

--- src/rf_test.c
+++ src/rf_test.c
@@ -67,7 +67,7 @@ void classRFIsingGraph(double *x, int *dimx, int *cat, int *maxcat,
 		zeroDouble(counttrnew, 4 * nsample);
 		
 		double *xnew      = (double *) S_alloc(xdimCount* nsample, sizeof(double));
-		int *ynew 	  = (double *) S_alloc(nsample, sizeof(double));
+		int *ynew 	  = (int *) S_alloc(nsample, sizeof(int));
 		
 		 for(int i=0; i< (mdim-1);i++){
 			 for (int j=i+1;j<mdim;j++){
