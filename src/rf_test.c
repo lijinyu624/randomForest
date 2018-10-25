73ab0b74bc9486db68a3e08f26203d3bc3fcec99
lijinyu624 <lijinyu624@gmail.com>
2018-10-25 14:27:04 -0500

    Update src/rf_test.c

--- src/rf_test.c
+++ src/rf_test.c
@@ -251,7 +251,7 @@ void classRFIsingGraph(double *x, int *dimx, int *cat, int *maxcat,
 		        colsum[n]+=counttr[nsample*j+n];
 		      }
 		    }
-		    for (int n=0; n<nsample;n++) Rprintf("csum:%d,",colsum[n]);
+		    for (int n=0; n<nsample;n++) Rprintf("%d,",colsum[n]);
 		    
 		    //write the out matrix as 4 x n. if ncl < 4, fill the rows with 0.
 			for (int n=0; n<nsample;n++){
